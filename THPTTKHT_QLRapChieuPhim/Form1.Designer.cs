ea070b915dd184709c8095f06c74684e10b39321
Kotatstu <kotatstu0303@gmail.com>
2025-05-23 15:51:57 +0700

    Basic homepage update

--- THPTTKHT_QLRapChieuPhim/Form1.Designer.cs
+++ THPTTKHT_QLRapChieuPhim/Form1.Designer.cs
@@ -28,12 +28,143 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 25.8000011F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(73, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(661, 50);
+            label1.TabIndex = 0;
+            label1.Text = "Ứng dụng quản lý rạp chiếu phim";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(226, 171);
+            button1.Name = "button1";
+            button1.Size = new Size(104, 49);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(350, 171);
+            button2.Name = "button2";
+            button2.Size = new Size(104, 49);
+            button2.TabIndex = 1;
+            button2.Text = "button1";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(477, 171);
+            button3.Name = "button3";
+            button3.Size = new Size(104, 49);
+            button3.TabIndex = 1;
+            button3.Text = "button1";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(226, 226);
+            button4.Name = "button4";
+            button4.Size = new Size(104, 49);
+            button4.TabIndex = 1;
+            button4.Text = "button1";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(350, 226);
+            button5.Name = "button5";
+            button5.Size = new Size(104, 49);
+            button5.TabIndex = 1;
+            button5.Text = "button1";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(477, 226);
+            button6.Name = "button6";
+            button6.Size = new Size(104, 49);
+            button6.TabIndex = 1;
+            button6.Text = "button1";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(226, 281);
+            button7.Name = "button7";
+            button7.Size = new Size(104, 49);
+            button7.TabIndex = 1;
+            button7.Text = "button1";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(350, 281);
+            button8.Name = "button8";
+            button8.Size = new Size(104, 49);
+            button8.TabIndex = 1;
+            button8.Text = "button1";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(477, 281);
+            button9.Name = "button9";
+            button9.Size = new Size(104, 49);
+            button9.TabIndex = 1;
+            button9.Text = "button1";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(825, 491);
+            Controls.Add(button9);
+            Controls.Add(button6);
+            Controls.Add(button3);
+            Controls.Add(button8);
+            Controls.Add(button5);
+            Controls.Add(button2);
+            Controls.Add(button7);
+            Controls.Add(button4);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
     }
 }
--- THPTTKHT_QLRapChieuPhim/Form1.cs
+++ THPTTKHT_QLRapChieuPhim/Form1.cs
@@ -6,5 +6,10 @@ namespace THPTTKHT_QLRapChieuPhim
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
